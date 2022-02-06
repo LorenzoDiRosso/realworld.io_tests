b2073502dfe30cb4655e878f4522696a85529d91
Mike Sell <michael.sell2@mail.dcu.ie>
2022-02-06 15:12:44 -0300

    base.py is done, selenium warnings fixed

--- Base/base.py
+++ Base/base.py
@@ -1,17 +1,19 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
 
 class Base:
 
     @pytest.fixture(autouse=True)
     def set_up(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
-
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('log-level=3')
         chrome_options.add_argument('headless')
         chrome_options.add_argument('window-size=1920x1080') #standard resolution
-        self.driver = webdriver.Chrome(options=chrome_options)
+        s=Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=s, options=chrome_options)
 
         print("-----------------------------------------")
         print("Test is started")

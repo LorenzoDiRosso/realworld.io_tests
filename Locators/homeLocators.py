974429f4e8cda20db5daa305e54197cb1553921f
Mike Sell <michael.sell2@mail.dcu.ie>
2022-02-06 15:14:05 -0300

    added locators

---
+++ Locators/homeLocators.py
@@ -0,0 +1,5 @@
+class HomeLocators:
+
+    # Home page elements
+    settings_link_text = 'Settings'
+    your_feed_link_text = 'Your Feed'
---
+++ Locators/registerLocators.py
@@ -0,0 +1,7 @@
+class RegisterLocators:
+
+    # Registration page elements
+    username_textbox_css = 'fieldset:nth-of-type(1) > .form-control.form-control-lg.ng-empty.ng-pristine.ng-untouched.ng-valid'
+    email_textbox_css = '.form-control.form-control-lg.ng-empty.ng-pristine.ng-untouched.ng-valid.ng-valid-email'
+    password_textbox_css = '.form-control.form-control-lg.ng-empty.ng-pristine.ng-untouched.ng-valid'
+    sign_up_button_xpath = "//button[@type='submit']"
